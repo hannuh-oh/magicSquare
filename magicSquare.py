121292bdf3c69d12d3853645533a31e48095a286
hannuh-oh <hgrant.f18@warren-wilson.edu>
2019-03-26 15:33:33 -0400

    first magic square

---
+++ magicSquare.py
@@ -0,0 +1,17 @@
+"""
+This program will take a list of numbers from a user,
+and determine whether or not the numbers make a magic
+square.
+"""
+
+def getMagicNums():
+    """
+    Takes input from user, stores in list
+    :return: magicnumbers
+    """
+
+    magicNums = []
+    done = False
+
+    while not done:
+        
